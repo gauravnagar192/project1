b9e5f9c0e6f163019f8ed82a6b93c8fff950b0ac
Gaurav Nagar <gauravnagar192@gmail.com>
2020-08-29 13:40:33 +0530

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname,'/public')));
 
